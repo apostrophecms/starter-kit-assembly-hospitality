d61c0e418c999c35339d244fd3c68c6fa4d2f073
Jed <vjeudy@protonmail.com>
2025-05-13 17:03:07 +0200

    removes rich-text configs

--- dashboard/modules/helper/index.js
+++ dashboard/modules/helper/index.js
@@ -1,14 +1,3 @@
-const baseTextToolbar = [
-  'Styles',
-  'Bold', 'Italic', 'Link', 'Unlink',
-  'Split', 'NumberedList', 'BulletedList'
-];
-const baseTextStyles = [
-  {
-    name: 'Body',
-    element: 'p'
-  }
-];
 const widgets = {
   gallery: {}
 };
@@ -16,13 +5,9 @@ const widgets = {
 export default {
   options: {
     alias: 'helpers',
-    baseTextStyles,
-    baseTextToolbar
   },
   helpers(self, options) {
     return {
-      baseTextStyles,
-      baseTextToolbar,
       widgets
     };
   }

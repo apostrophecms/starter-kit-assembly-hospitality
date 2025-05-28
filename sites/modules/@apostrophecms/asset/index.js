decced8d99a08741534b494a306604e370931434
Thomas Boutell <tom@apostrophecms.com>
2025-05-28 16:38:47 -0400

    fix PRO-7836, simple rich text widgets without double nesting

--- sites/modules/@apostrophecms/asset/index.js
+++ sites/modules/@apostrophecms/asset/index.js
@@ -1,7 +1,8 @@
 export default {
   options: {
     // When not in production, refresh the page on restart
-    refreshOnRestart: true
+    refreshOnRestart: true,
+    hmr: 'apos'
   },
   methods(self) {
     return {
